ee945c7dffb79945d9a4ae24a2d0f2b7d0bf7ac1
Vivek <vivekj011@gmail.com>
2017-05-13 14:47:18 +0530

    summary

--- src/views/callView.js
+++ src/views/callView.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import record from 'actions/record'
 import WaveformView from 'views/WaveformView'
+import Graph from 'containers/sentimentGraph'
+import {getFileData} from 'actions/common'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 if (!__SERVER__) {
@@ -12,7 +14,8 @@ class CallView extends Component {
     super(props)
     this.state = {
       calling: false,
-      generatingReport: false
+      generatingReport: false,
+      showSummary: false
     }
     this.onCall = this.onCall.bind(this)
   }
@@ -32,6 +35,17 @@ class CallView extends Component {
           this.setState({
             calling: false
           })
+          setTimeout(()=>{
+            getFileData().then((data)=>{
+              this.setState({
+                showSummary : true,
+                generatingReport: false,
+                sentimentData: data.sentimentData,
+                summary: data.summary 
+              })  
+            })
+            
+          }, 3000)
       })
       // onComplete setState
     }else {
@@ -47,7 +61,7 @@ class CallView extends Component {
         <div className='call-1 call-img'></div>
         <div className='user-1 call-img'></div>
         <div className={btnCls} onClick={this.onCall}></div>
-        { this.state.generatingReport &&
+        { this.state.generatingReport && !this.state.showSummary &&
           <ReactCSSTransitionGroup
             transitionName='md-example'
             transitionEnterTimeout={200}
@@ -57,6 +71,25 @@ class CallView extends Component {
           >
             <WaveformView ref={(node) => { this.wf = node }}/>
           </ReactCSSTransitionGroup>
+        }
+        { this.state.showSummary &&
+          <div className='sumry'>
+            <div className='smtr-hdr'>Report</div>
+            <div className='smtr-macow'></div>
+            <div>
+              23 sec (Call Time)
+            </div>
+            <div className='smry-dtls'>
+              <h3>Summary</h3>
+              <div className='smry-txt'>{this.state.summary}</div>
+            </div>
+            <div className='smry-analysis'>
+              <h3>Sentiment Analysis</h3>
+              <div className='smry-grph'>
+                <Graph sentimentData={this.state.sentimentData} fileName={'asd'} />
+              </div>
+            </div>
+          </div>
         }
       </div>
     )
